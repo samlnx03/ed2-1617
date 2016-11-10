ca167738a4eea0a1fc1a04b94fe9f58ce5992fd2
samuel pérez <samlnx03@hotmail.com>
2016-11-10 12:32:45 -0600

    Update inport.v

--- inport.v
+++ inport.v
@@ -1,37 +1,49 @@
 module inport #(parameter WIDTH=8) 
-	(input oe, clk, ok2send, output reg ready, output [WIDTH-1:0]q);
+	(input reset, oe, ok2send, output reg stopSeq, output [WIDTH-1:0]q);
+	// stopSeq es una señal para detener el secuenciador
+	// 		sube al subir oe y baja al subir ok2send
+	// ok2send se debe subir para indicar que el dato esta listo para ser leido
 	reg [WIDTH-1:0] valor=5;
-
-	parameter [1:0] INICIAL=3'b00,
-			WAIT_OK2SEND=3'b01,
-			WAIT_NOT_OE=3'b10;
-	reg [1:0] state, next;
 
 	assign q = (oe) ? valor : {WIDTH{1'bz}};
 	// el valor sera wire y se cambia con el teclado
-
-	// maq de estado
-	always @(posedge clk) begin
-		state=next;
-	end
-	always @(*) begin
-		case (state)
-			INICIAL: begin
-				if(~oe) begin next=INICIAL; ready=1'b0; end
-				else if(~ok2send) begin next=WAIT_OK2SEND; ready=1'b1; end
-				end
-			WAIT_OK2SEND: begin
-				if(!oe) begin next=INICIAL; ready=1'b0; end  // reset?
-				else if(~ok2send) begin next=WAIT_OK2SEND; ready=1'b1; end
-				else begin next=WAIT_NOT_OE; ready=1'b1; end
-				end
-			WAIT_NOT_OE: begin
-				if(oe) begin next=WAIT_NOT_OE; ready=1'b0; end
-				else begin next=INICIAL; ready=1'b0; end
-				end
-			default: next=INICIAL;
-		endcase
-
+	
+	assign ok=ok2send | reset;
+	always @(posedge oe, posedge ok) begin
+		if(ok2send)
+			stopSeq<=1'b0;
+		else
+			stopSeq<=1'b1;
 	end
 endmodule
 
+
+module inport_tb();
+	reg clk;
+	reg reset, oe, ok2send;
+	wire stopSeq;
+	wire [7:0]q;
+
+	inport INP1(reset, oe,ok2send,stopSeq,q);
+
+	initial begin
+		$monitor("t=%3d, clk=%b, oe=%b, ok2send=%b, -- stopSeq=%b, q=%d",
+			$time, clk, oe, ok2send, stopSeq, q);
+		reset=0; oe=0; clk=0; ok2send=0;
+	end
+	initial forever #10 clk=~clk;
+	initial #100 $finish;
+	initial begin
+		#31 $display("\nok2send antes de 1 ciclo");
+		oe=1;
+		#5 ok2send=1; #2 ok2send=0;
+		#13 oe=1'b0;
+	end
+
+	initial begin
+		#71 $display("\nok2send despues de 1 ciclo");
+		oe=1;
+		#20 ok2send=1; #2 ok2send=0;
+		#13 oe=1'b0;
+	end
+endmodule
