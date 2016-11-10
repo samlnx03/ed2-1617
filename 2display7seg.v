c7575a01be665ea6011a6daee7bb8b5de3c14b60
samuel pérez <samlnx03@hotmail.com>
2016-11-10 12:35:25 -0600

    Update 2display7seg.v

--- 2display7seg.v
+++ 2display7seg.v
@@ -14,7 +14,7 @@ endmodule
 module decBin7seg(input [3:0]a, output reg[6:0]s);
   always @(*) begin
   case (a)
-					// abcdefg   segmentos catodos
+	       // abcdefg   segmentos catodos
     4'b0000: s=7'b0000001;
     4'b0001: s=7'b1001111;
     4'b0010: s=7'b0010010;
@@ -25,12 +25,12 @@ module decBin7seg(input [3:0]a, output reg[6:0]s);
     4'b0111: s=7'b1110000;
     4'b1000: s=7'b0000000;
     4'b1001: s=7'b0001100;
-	 4'd10:	 s=7'b0001000;		//a
-	 4'd11:	 s=7'b1100000;		//b
-	 4'd12:	 s=7'b0110001;		//c
-	 4'd13:	 s=7'b1000010;		//d
-	 4'd14:	 s=7'b0110000;		//e
-	 4'd15:	 s=7'b0111000;		//f
+	4'd10:	 s=7'b0001000;		//a
+	4'd11:	 s=7'b1100000;		//b
+	4'd12:	 s=7'b0110001;		//c
+	4'd13:	 s=7'b1000010;		//d
+	4'd14:	 s=7'b0110000;		//e
+	4'd15:	 s=7'b0111000;		//f
   endcase
   end
 endmodule
